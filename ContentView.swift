308bbce42ab9de4467b10fa2779ea962830e45a3
Soumya Darshan Kamila <113112541+soumyaDKamila@users.noreply.github.com>
2023-05-05 20:13:38 +0530

    add-file

---
+++ ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by Soumya Darshan Kamila on 27/04/23.
+//
+
+import SwiftUI
+
+enum Emoji: String, CaseIterable {
+    case 👇🏻, 😂, 🎉, 🚀
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .🎉
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Soumya Darshan Kamila on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
